bc20161ced2e46ed7a20cd5ef33dd2aca96aeb8a
Rohan Mohanty <mohantyrohan3@gmail.com>
2026-04-14 00:37:16 +0530

    Initial commit

---
+++ recurringService/src/main/java/com/cventro/recurringService/RecurringServiceApplication.java
@@ -0,0 +1,15 @@
+package com.cventro.recurringService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class RecurringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecurringServiceApplication.class, args);
+	}
+
+}
